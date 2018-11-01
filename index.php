4bde07a107c62b66fd1de9cad92485819391dc56
RyuichiPaulEgoshi <dream.come.true48694062@gmail.com>
2018-11-01 08:11:59 +0800

    adding all files

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Main Page</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="styles/style.css" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <script src="main.js"></script>
+</head>
+<body>
+    <!--Left Navigation Bars-->
+    <div class="nav-container">
+        <ul>
+            <li><a href="#"><i class="material-icons">home</i></a></li>
+            <li><a href="#"><i class="material-icons">person_outline</i></a></li>
+            <li><a href="#"><i class="material-icons">person_outline</i></a></li>
+            <li><a href="#"><i class="material-icons">school</i></a></li>
+            <li id="down-item"><a href="#"><i class="material-icons">settings</i></a></li>
+        </ul>
+    </div>
+
+    <!--index-->
+    <div class="index-container">
+    </div>
+</body>
+</html>
