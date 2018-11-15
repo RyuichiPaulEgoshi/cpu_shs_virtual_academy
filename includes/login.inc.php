92801adaf4a4f0dc59fbdb18a63faa71394b1dd1
RyuichiPaulEgoshi <dream.come.true48694062@gmail.com>
2018-11-15 13:34:55 +0800

    -a

---
+++ includes/login.inc.php
@@ -0,0 +1,32 @@
+<?php
+
+session_start();
+
+include 'db.inc.php';
+
+function account(
+    
+)
+
+switch (true) {
+
+    #Admin account
+    case (isset($_POST['stu-acc'])):
+        break;
+    
+    #Teacher account
+    case (isset($_POST['tea-acc'])):
+        break;
+    
+    #Student account
+    case (isset($_POST['stu-acc'])):
+        break;
+
+    #Parent account
+    case (isset($_POST['par-acc'])):
+        break;
+    
+    #System Error
+    default:
+        break;
+}
