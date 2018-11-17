10725bc60426db963fa3cb57ec071f7282ab17ef
RyuichiPaulEgoshi <dream.come.true48694062@gmail.com>
2018-11-17 13:19:37 +0800

    -a

--- script/script.js
+++ script/script.js
@@ -19,11 +19,9 @@ function CloseNav() {
     "-350px";
 }
 
-//Open Nav1
+//OpenClose Nav1
 function ShowNav1(){
     if (navStatus == true) {
-        document.getElementById("nav1").style.left =
-        "50px";
         document.getElementById("nav2").style.left =
         "-350px";
         document.getElementById("nav3").style.left =
@@ -38,8 +36,186 @@ function ShowNav1(){
         "-350px";
         document.getElementById("nav8").style.left =
         "-350px";
+        document.getElementById("nav1").style.left =
+        "50px";
     } else if (navStatus == false) {
         document.getElementById("nav1").style.left =
+        "-350px";
+    }
+}
+
+
+//OpenClose Nav2
+function ShowNav2(){
+    if (navStatus == true) {
+        document.getElementById("nav1").style.left =
+        "-350px";
+        document.getElementById("nav3").style.left =
+        "-350px";
+        document.getElementById("nav4").style.left =
+        "-350px";
+        document.getElementById("nav5").style.left =
+        "-350px";
+        document.getElementById("nav6").style.left =
+        "-350px";
+        document.getElementById("nav7").style.left =
+        "-350px";
+        document.getElementById("nav8").style.left =
+        "-350px";
+        document.getElementById("nav2").style.left =
+        "50px";
+    } else if (navStatus == false) {
+        document.getElementById("nav2").style.left =
+        "-350px";
+    }
+}
+
+//OpenClose Nav3
+function ShowNav3(){
+    if (navStatus == true) {
+        document.getElementById("nav1").style.left =
+        "-350px";
+        document.getElementById("nav2").style.left =
+        "-350px";
+        document.getElementById("nav4").style.left =
+        "-350px";
+        document.getElementById("nav5").style.left =
+        "-350px";
+        document.getElementById("nav6").style.left =
+        "-350px";
+        document.getElementById("nav7").style.left =
+        "-350px";
+        document.getElementById("nav8").style.left =
+        "-350px";
+        document.getElementById("nav3").style.left =
+        "50px";
+    } else if (navStatus == false) {
+        document.getElementById("nav3").style.left =
+        "-350px";
+    }
+}
+
+//OpenClose Nav4
+function ShowNav4(){
+    if (navStatus == true) {
+        document.getElementById("nav1").style.left =
+        "-350px";
+        document.getElementById("nav2").style.left =
+        "-350px";
+        document.getElementById("nav3").style.left =
+        "-350px";
+        document.getElementById("nav5").style.left =
+        "-350px";
+        document.getElementById("nav6").style.left =
+        "-350px";
+        document.getElementById("nav7").style.left =
+        "-350px";
+        document.getElementById("nav8").style.left =
+        "-350px";
+        document.getElementById("nav4").style.left =
+        "50px";
+    } else if (navStatus == false) {
+        document.getElementById("nav4").style.left =
+        "-350px";
+    }
+}
+
+//OpenClose Nav5
+function ShowNav5(){
+    if (navStatus == true) {
+        document.getElementById("nav1").style.left =
+        "-350px";
+        document.getElementById("nav2").style.left =
+        "-350px";
+        document.getElementById("nav3").style.left =
+        "-350px";
+        document.getElementById("nav4").style.left =
+        "-350px";
+        document.getElementById("nav6").style.left =
+        "-350px";
+        document.getElementById("nav7").style.left =
+        "-350px";
+        document.getElementById("nav8").style.left =
+        "-350px";
+        document.getElementById("nav5").style.left =
+        "50px";
+    } else if (navStatus == false) {
+        document.getElementById("nav5").style.left =
+        "-350px";
+    }
+}
+
+//OpenClose Nav6
+function ShowNav6(){
+    if (navStatus == true) {
+        document.getElementById("nav1").style.left =
+        "-350px";
+        document.getElementById("nav2").style.left =
+        "-350px";
+        document.getElementById("nav3").style.left =
+        "-350px";
+        document.getElementById("nav4").style.left =
+        "-350px";
+        document.getElementById("nav5").style.left =
+        "-350px";
+        document.getElementById("nav7").style.left =
+        "-350px";
+        document.getElementById("nav8").style.left =
+        "-350px";
+        document.getElementById("nav6").style.left =
+        "50px";
+    } else if (navStatus == false) {
+        document.getElementById("nav6").style.left =
+        "-350px";
+    }
+}
+
+//OpenClose Nav7
+function ShowNav7(){
+    if (navStatus == true) {
+        document.getElementById("nav1").style.left =
+        "-350px";
+        document.getElementById("nav2").style.left =
+        "-350px";
+        document.getElementById("nav3").style.left =
+        "-350px";
+        document.getElementById("nav4").style.left =
+        "-350px";
+        document.getElementById("nav5").style.left =
+        "-350px";
+        document.getElementById("nav6").style.left =
+        "-350px";
+        document.getElementById("nav8").style.left =
+        "-350px";
+        document.getElementById("nav7").style.left =
+        "50px";
+    } else if (navStatus == false) {
+        document.getElementById("nav7").style.left =
+        "-350px";
+    }
+}
+
+//OpenClose Nav8
+function ShowNav8(){
+    if (navStatus == true) {
+        document.getElementById("nav1").style.left =
+        "-350px";
+        document.getElementById("nav2").style.left =
+        "-350px";
+        document.getElementById("nav3").style.left =
+        "-350px";
+        document.getElementById("nav4").style.left =
+        "-350px";
+        document.getElementById("nav5").style.left =
+        "-350px";
+        document.getElementById("nav6").style.left =
+        "-350px";
+        document.getElementById("nav7").style.left =
+        "-350px";
+        document.getElementById("nav8").style.left =
+        "50px";
+    } else if (navStatus == false) {
+        document.getElementById("nav8").style.left =
         "-350px";
     }
 }
