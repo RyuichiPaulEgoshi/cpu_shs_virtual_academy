dc90ca223019cc963f240c4c84379482a2761721
RyuichiPaulEgoshi <dream.come.true48694062@gmail.com>
2018-11-19 13:44:21 +0800

    -a

--- common/nav.com.php
+++ common/nav.com.php
@@ -34,19 +34,26 @@ case (true):
 <!--Main Navigation Bars-->
 <div class="nav-container">
     <ul>
-        <li onmousemove="ShowNav1()"><a href="#"><i class="material-icons">default</i></a></li>
+        <!--1 home-->
+        <li onmousemove="ShowNav1()"><a href="index1_home.php"><i class="material-icons">default</i></a></li>
+        <!--2 account-->
         <li onmousemove="ShowNav2()"><a href="#"><i class="material-icons">default</i></a></li>
+        <!--3 class-->
         <li onmousemove="ShowNav3()"><a href="#"><i class="material-icons">default</i></a></li>
+        <!--4 resources-->
         <li onmousemove="ShowNav4()"><a href="#"><i class="material-icons">default</i></a></li>
+        <!--5 transaction-->
         <li onmousemove="ShowNav5()"><a href="#"><i class="material-icons">default</i></a></li>
+        <!--6 resources-->
         <li onmousemove="ShowNav6()"><a href="#"><i class="material-icons">default</i></a></li>
+        <!--7 settings-->
         <li onmousemove="ShowNav7()"><a href="#"><i class="material-icons">default</i></a></li>
         <li onmousemove="ShowNav8()"><a href="#"><i class="material-icons">default</i></a></li>
     </ul>
 </div>
 
 <!--Sub Navigation Bars-->
-<div id="home-nav" class="home-nav">
+<div id="nav1" class="nav1">
     <h2>HOME</h2>
     <i class="material-icons" id="closeNav"  onclick="CloseNav()">backspace</i>
 </div>
