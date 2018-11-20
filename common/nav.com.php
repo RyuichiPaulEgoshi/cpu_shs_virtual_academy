0710f8ed49080cdd427b1db4b7b2e05439fb714e
RyuichiPaulEgoshi <dream.come.true48694062@gmail.com>
2018-11-20 12:08:42 +0800

    -a

--- common/nav.com.php
+++ common/nav.com.php
@@ -57,6 +57,7 @@ case (true):
 
 <!--Sub 1 Home-->
 <div id="nav1" class="nav1">
+    <h2>HOME</h2>
     <i class="material-icons" id="closeNav"  onclick="CloseNav()">backspace</i>
 </div>
 
@@ -67,6 +68,17 @@ case (true):
 
 <!--Sub 3 Class-->
 <div id="nav3" class="nav3">
+    <?php
+        include 'includes/db.inc.php';
+
+        $sql1 = "SELECT acc_href, acc_omm, acc_name FROM _access WHERE acc_par = 3";
+        $result1 = mysqli_query($conn, $sql1);
+        while($row1 = mysqli_fetch_array($result1)):;
+    ?>
+    
+    <?php
+        endwhile;
+    ?>
     <i class="material-icons" id="closeNav"  onclick="CloseNav()">backspace</i>
 </div>
 
@@ -77,15 +89,18 @@ case (true):
 
 <!--Sub 5 Transaction-->
 <div id="nav5" class="nav5">
+    <h2>TRANSACTIONS</h2>
     <i class="material-icons" id="closeNav"  onclick="CloseNav()">backspace</i>
 </div>
 
 <!--Sub 6 Settings-->
 <div id="nav6" class="nav6">
+    <h2>SETTINGS</h2>
     <i class="material-icons" id="closeNav"  onclick="CloseNav()">backspace</i>
 </div>
 
 <!--Sub 7 Help-->
 <div id="nav7" class="nav7">
+    <h2>HELP</h2>
     <i class="material-icons" id="closeNav"  onclick="CloseNav()">backspace</i>
 </div>
