84eec6dc290dbcdfdf0b03c6d01401fed25135a2
RyuichiPaulEgoshi <dream.come.true48694062@gmail.com>
2018-11-26 13:37:11 +0800

    -a

--- common/dbscripts.com.php
+++ common/dbscripts.com.php
@@ -1,5 +1,7 @@
 <?php
+#This common part is for SQL
 
+#User Account
 $sql1 = '';
 $sql2 = '';
 $sql3 = '';
@@ -9,5 +11,16 @@ $sql6 = '';
 $sql7 = '';
 $sql8 = '';
 $sql9 = '';
-$sql9 = '';
+$sql10 = '';
+$sql11 = '';
+$sql12 = '';
+$sql13 = '';
+$sql14 = '';
+$sql15 = '';
+$sql16 = '';
+$sql17 = '';
+$sql18 = '';
+$sql19 = '';
+$sql20 = '';
+
 
